b213f15e515d70d96a951a3e24ab631f2ad7a8b5
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 23:10:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(in_line)
   else
      result="The line is currently: "
      in_line.each do |customer|
-       result += "#{in_line.index(customer)+1}.#{customer}"
+       result += " #{in_line.index(customer)+1}. #{customer}"
      end
      puts result
    end
