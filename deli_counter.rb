da6f1b0b5d50977b4d415dddc2c9d5278c0b3cab
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 23:34:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(in_line)
    end
 end
 def take_a_number(in_line, customer)
-    in_line.push(customer)
+    in_line<<customer
       puts "Welcome, #{customer}. You are number #{in_line.size} in line." if in_line.size > 1
 end
 def now_serving(in_line)
