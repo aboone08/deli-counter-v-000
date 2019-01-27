b15e78d95cce7878b104a0cdb8cd2b53621f3406
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 21:52:44 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,16 @@
+# Write your code here.
+katz_deli = []
+def line(in_line)
+  if in_line.size==0
+  puts "The line is currently empty."
+  else
+
+def take_a_number(in_line, customer)
+      puts "#{customer} is the #{in_line} person in line."
+end
+def now_serving(in_line)
+  puts "Now serving #{customer}."
+  in_line.pull(customer)
+  puts "There is nobody waiting to be served!" if line == 0
+     end
+end
