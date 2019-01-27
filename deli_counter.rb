1a6875d1bbc3590fb11211c2f2848465bd47fed4
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 22:10:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(in_line)
   else
     customer.to_i
     customer.push(in_line)
-    puts "The line has #{in_line} number of people in line." 
+    puts "The line has #{in_line} number of people in line."
  take_a_number(in_line, customer)
       puts "#{customer} is the #{in_line} person in line." if in_line.size > 1
 end
