8188d71791d9a81add6328a330a780e2c046d208
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 21:58:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(in_line)
   puts "The line is currently empty."
 end
 def take_a_number(in_line, customer)
-      puts "#{customer} is the #{in_line} person in line."
+  customer.to_i 
+      puts "#{customer} is the #{in_line} person in line." if in_line.size > 1
 end
 def now_serving(in_line)
   puts "Now serving #{customer}."
