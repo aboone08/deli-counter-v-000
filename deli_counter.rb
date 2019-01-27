100348cab2926d3c0712a90d89179768f48d9fcc
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 22:40:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,7 @@ def line(in_line)
   if in_line.size==0
   puts "The line is currently empty."
   else
-    in_line.push(katz_deli)
-    katz_deli.index
+    in_line.push(katz_deli).index
       puts "The line is currently: #{in_line}."
 
 end
