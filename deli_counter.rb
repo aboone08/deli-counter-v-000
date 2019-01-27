b53c1c7a1699a9d01ad977559aceabf403f39426
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 21:30:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,15 @@
 # Write your code here.
 katz_deli = []
-def line(place)
-  puts "The line is currently empty." if line = 0
+def line(katz_deli)
+  puts "The line is currently empty." if line == 0
 end
 def take_a_number(katz_deli, customer)
   katz_deli.to_i
   puts "#{customer} is the #{katz_deli} person in line."
 end
+def now_serving(katz_deli)
+  puts "Now serving #{customer}."
+  katz_deli.pull 
+  puts "There is nobody waiting to be served!" if line == 0 
+  
+end 
