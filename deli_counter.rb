38f2c37f860dd4efe396ef70b38c17caba493874
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 22:47:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(in_line)
   puts "The line is currently empty."
   else
     in_line.push(katz_deli)
-      puts "The line is currently:" in_line
+     "The line is currently:" in_line
 end
 def take_a_number(in_line, customer)
       puts "Welcome, #{customer}. You are number #{line} in line." if in_line.size > 1
