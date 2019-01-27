b22eb4741bea4fa7988cf452b2b7c4b3acf7d1e2
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 21:25:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+katz_deli = []
+def line(place)
+  puts "The line is currently empty." if line = 0
+end
+def take_a_number(katz_deli, customer)
+  puts "#{customer} is the #{katz_deli} person in line."
+end
