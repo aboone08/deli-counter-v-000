a1f3d3fbeaa5a3f5c09cef2063f851733358defa
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 22:05:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ katz_deli = []
 def line(in_line)
   if in_line.size==0
   puts "The line is currently empty."
-end
-def take_a_number(in_line, customer)
+else
+ take_a_number(in_line, customer)
   customer.to_i
   customer.push(in_line)
       puts "#{customer} is the #{in_line} person in line." if in_line.size > 1
